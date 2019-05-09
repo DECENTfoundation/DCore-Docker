aebe16e0973d3414779e479c910190ab76009163
Richard Fric <richard.fric@decent.ch>
2019-05-09 09:59:10 +0200

    Correct PBC build

--- centos/pbc.sh
+++ centos/pbc.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-[ $# -lt 2 ] && { echo "Usage: $0 image_version pbc_version [git_revision]"; exit 1; }
+[ $# -lt 2 ] && { echo "Usage: $0 image_version pbc_version [git_revision] [packages_dir]"; exit 1; }
 
 PBC_VERSION=$2
+BASE_IMAGE=centos
+
 if [ $# -lt 3 ]; then GIT_REV=$2; else GIT_REV=$3; fi
 
-docker run -it -w /root --rm --name centos.pbc.$1 \
-    --mount type=bind,src=$PWD/packages,dst=/root/rpmbuild/RPMS/x86_64 \
-    --mount type=bind,src=$PWD/centos,dst=/root/centos,readonly \
-    centos:$1 centos/pbc-build.sh $PBC_VERSION $GIT_REV
+if [ $# -lt 4 ]; then PACKAGES_DIR="$PWD/packages/$BASE_IMAGE/$IMAGE_VERSION"; else PACKAGES_DIR=$4; fi
+
+mkdir -p $PACKAGES_DIR
+docker run -w /root --rm --name $BASE_IMAGE.pbc.$1 \
+    --mount type=bind,src=$PACKAGES_DIR,dst=/root/rpmbuild/RPMS/x86_64 \
+    --mount type=bind,src=$PWD/$BASE_IMAGE,dst=/root/$BASE_IMAGE,readonly \
+    $BASE_IMAGE:$1 $BASE_IMAGE/pbc-build.sh $PBC_VERSION $GIT_REV
--- fedora/pbc.sh
+++ fedora/pbc.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-[ $# -lt 2 ] && { echo "Usage: $0 image_version pbc_version [git_revision]"; exit 1; }
+[ $# -lt 2 ] && { echo "Usage: $0 image_version pbc_version [git_revision] [packages_dir]"; exit 1; }
 
 PBC_VERSION=$2
+BASE_IMAGE=fedora
+
 if [ $# -lt 3 ]; then GIT_REV=$2; else GIT_REV=$3; fi
 
-docker run -it -w /root --rm --name fedora.pbc.$1 \
-    --mount type=bind,src=$PWD/packages,dst=/root/rpmbuild/RPMS/x86_64 \
-    --mount type=bind,src=$PWD/fedora,dst=/root/fedora,readonly \
-    fedora:$1 fedora/pbc-build.sh $PBC_VERSION $GIT_REV
+if [ $# -lt 4 ]; then PACKAGES_DIR="$PWD/packages/$BASE_IMAGE/$IMAGE_VERSION"; else PACKAGES_DIR=$4; fi
+
+mkdir -p $PACKAGES_DIR
+docker run -w /root --rm --name $BASE_IMAGE.pbc.$1 \
+    --mount type=bind,src=$PACKAGES_DIR,dst=/root/rpmbuild/RPMS/x86_64 \
+    --mount type=bind,src=$PWD/$BASE_IMAGE,dst=/root/$BASE_IMAGE,readonly \
+    $BASE_IMAGE:$1 $BASE_IMAGE/pbc-build.sh $PBC_VERSION $GIT_REV
